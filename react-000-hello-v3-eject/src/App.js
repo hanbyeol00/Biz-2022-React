a441614abf3b71357b61623a9e8aeea020a8868f
hanbyeol00 <gksquf6670@gmail.com>
2022-12-15 09:10:24 +0900

    수정

--- react-000-hello-v3-eject/src/App.js
+++ react-000-hello-v3-eject/src/App.js
@@ -31,3 +31,18 @@ const App = () => {
 };
 
 export default App;
+/**
+ * MVC(Model View Controller)
+ * 1. Model View Controller 패턴은 대부분 패턴의 시초
+ * 2. Model은 데이터, 상태, 비즈니스를 나타낸다
+ * 3. View는 사용자에게 보여줄 UI와 컨트롤러와 연동
+ * 4. Controller(Router)는 동작을 그린다
+ * 5. Controller에는 생명주기를 작성, Model(객체)과 View를 가져다가 사용
+ * MVP(Model View Presenter)
+ * 1. 컨트롤러 중심 부분을 개선, 뷰는 컨트롤러가 가진 화면 구성을 대신, 화면 구성 단에
+ *    인터페이스를 가져와 사용해 View 단위 테스트를 가능
+ * 2. 프리젠터는 각 기능을 인터페이스 화 해서 센터에서 메소드 별 분할 관리가 가능
+ * MVVM(Model View ViewModel)
+ * 1. View와 ViewModel 사이에 데이터 바인딩을 사용
+ * 2. ViewModel 의 상태에 따라 View 를 갱신
+ */
--- react-000-hello-v3/src/App.js
+++ react-000-hello-v3/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import Button from "./comps/Button";
 import Box from "./comps/Box";
 import Section from "./comps/Section";
+import Article from "./comps/Article";
 
 const App = () => {
   const buttonStyle = {
@@ -11,6 +12,7 @@ const App = () => {
     color: "white",
     padding: "12px 16px",
   };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -26,8 +28,23 @@ const App = () => {
         </Button>
         <Box />
       </Section>
+      <Article theme="yellow"></Article>
     </div>
   );
 };
 
 export default App;
+/**
+ * 데이터 영역(Data segment), 또는 Static 영역
+ * 1. 애플리케이션이 실행되면서 필요한 변수가 저장되는 영역
+ * 2. 애플리케이션이 구동되는 동안 항상 접근 가능 한 영역
+ * 3. 클래스 영역에 선언된 전역변수 와 정적변수 영역
+ * 스택 영역(Stack segment)
+ * 1. 메서드, 함수 내에 정의된 지역변수가 저장되는 영역
+ * 2. 메서드, 함수가 호출될때 할당되고 종료될 때 해제된다.
+ * 힙영역(Heap segment)
+ * 1. 동적 할당영역
+ * 2. 인스턴트, 배열 등 참조 변수들이 저장되는 영역
+ * 3. 객체 배열을 생성하면 실제 데이터는 Heap에 저장되고
+ *    저장소의 주소는 스택에 저장된다
+ */
