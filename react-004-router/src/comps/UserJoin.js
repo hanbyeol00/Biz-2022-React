12b5ea423ac41588ae8bfbd2fced60362888793f
hanbyeol00 <gksquf6670@gmail.com>
2022-12-21 15:31:37 +0900

    React Router

--- react-004-router/src/comps/UserJoin.js
+++ react-004-router/src/comps/UserJoin.js
@@ -1,4 +1,12 @@
+import { useParams } from "react-router-dom";
 const UserJoin = () => {
-  return <h1>여기는 회원가입 화면입니다</h1>;
+  const { username } = useParams();
+
+  return (
+    <>
+      <h1>여기는 회원가입 화면입니다</h1>
+      <p>username:{username}</p>
+    </>
+  );
 };
 export default UserJoin;
