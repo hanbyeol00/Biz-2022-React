4649a558030d80c9f468eb7186180f8d4c3ea573
hanbyeol00 <gksquf6670@gmail.com>
2022-12-21 15:24:50 +0900

    React Router

---
+++ react-004-router/src/comps/NoticeWrite.js
@@ -0,0 +1,4 @@
+const NoticeWrite = () => {
+  return <input placeholder="공지사항" />;
+};
+export default NoticeWrite;
---
+++ react-004-router/src/comps/UserLogin.js
@@ -0,0 +1,4 @@
+const UserLogin = () => {
+  return <h1>여기는 Login 화면</h1>;
+};
+export default UserLogin;
